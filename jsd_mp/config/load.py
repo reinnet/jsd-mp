ce765e03a51db10fd4378bc5ea69b92fa7fc0984
Parham Alvani <parham.alvani@gmail.com>
2020-04-29 09:33:09 +0430

    feat: Load the Configuration with Test

--- jsd_mp/config/load.py
+++ jsd_mp/config/load.py
@@ -1,11 +1,19 @@
 import os
 import yaml
 import typing
+import dataclasses
 
-from jsd_mp.domain import Chain, Type, Direction, Link
+from jsd_mp.domain import Chain, Type, Direction, Link, VNFM
 
 
-def load(directory: str) -> typing.List[Chain]:
+@dataclasses.dataclass(frozen=True)
+class Config:
+    types: typing.Dict[str, Type]
+    chains: typing.List[Chain]
+    vnfm: VNFM
+
+
+def load(directory: str) -> Config:
     """
     load configuration files for:
     - physical toplogy (topology.yml)
@@ -14,26 +22,29 @@ def load(directory: str) -> typing.List[Chain]:
     - vnfm (vnfm.yml)
     """
     types: typing.Dict[str, Type] = {}
+    chains: typing.List[Chain] = []
 
     topology_yml = {}
     chains_yml = {}
-    vnf_yml = {}
+    types_yml = {}
     vnfm_yml = {}
 
     with os.scandir(directory) as entries:
         for entry in entries:
             if entry.is_file() and entry.name in (
-                "topology.yaml",
-                "chains.yaml",
-                "types.yaml",
-                "vnfm.yaml",
+                "topology.yml",
+                "chains.yml",
+                "types.yml",
+                "vnfm.yml",
             ):
                 with open(entry.path, "r") as f:
                     c = yaml.load(f, Loader=yaml.Loader)
-                    if entry.name == "chains.yaml":
+                    if entry.name == "chains.yml":
                         chains_yml = c["chains"]
-                    if entry.name == "types.yaml":
+                    if entry.name == "types.yml":
                         types_yml = c["types"]
+                    if entry.name == "vnfm.yml":
+                        vnfm_yml = c
     # build structure from loaded yamls
     for t in types_yml:
         types[t["name"]] = Type(
@@ -46,6 +57,7 @@ def load(directory: str) -> typing.List[Chain]:
             else Direction.EGRESS
             if t.get("egress", False)
             else Direction.NONE,
+            manageable=t.get("manageable", True),
         )
     for i, c in enumerate(chains_yml):
         chain = Chain(name=f"ch-{i}", fee=c["cost"])
@@ -56,6 +68,14 @@ def load(directory: str) -> typing.List[Chain]:
         for l in c["links"]:
             chain.add_link(l["source"], l["destination"], Link(l["bandwidth"]))
 
+        chains.append(chain)
+    vnfm = VNFM(
+        cores=vnfm_yml["cores"],
+        memory=vnfm_yml["ram"],
+        capacity=vnfm_yml["capacity"],
+        bandwidth=vnfm_yml["bandwidth"],
+        radius=vnfm_yml["radius"],
+        license_cost=vnfm_yml["licenseFee"],
+    )
 
-if __name__ == "__main__":
-    load("../simulation/config")
+    return Config(types, chains, vnfm)
---
+++ jsd_mp/config/test_load.py
@@ -0,0 +1,29 @@
+from .load import load
+from jsd_mp.domain import Type, Direction
+
+
+def test_laod():
+    cfg = load("config_example")
+
+    assert "ingress" in cfg.types
+    assert cfg.types["ingress"].memory == 0
+    assert cfg.types["ingress"].cores == 0
+    assert cfg.types["ingress"].direction == Direction.INGRESS
+    assert not cfg.types["ingress"].manageable
+
+    assert len(cfg.chains) == 1
+    assert cfg.chains[0].fee == 181
+    assert cfg.chains[0].functions == [
+        cfg.types["ingress"],
+        cfg.types["vDPI"],
+        cfg.types["vDPI"],
+        cfg.types["vNAT"],
+        cfg.types["egress"],
+    ]
+
+    assert cfg.vnfm.memory == 4
+    assert cfg.vnfm.cores == 2
+    assert cfg.vnfm.capacity == 10
+    assert cfg.vnfm.radius == 100
+    assert cfg.vnfm.bandwidth == 1
+    assert cfg.vnfm.license_cost == 100
