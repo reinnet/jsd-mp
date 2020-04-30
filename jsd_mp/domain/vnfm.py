623e67c685debb35b3c4f6dc13b94e722a8e140f
Parham Alvani <parham.alvani@gmail.com>
2020-04-30 21:39:06 +0430

    feat: Add Documentation for VNFM

--- jsd_mp/domain/vnfm.py
+++ jsd_mp/domain/vnfm.py
@@ -3,6 +3,12 @@ import dataclasses
 
 @dataclasses.dataclass(frozen=True)
 class VNFM:
+    """
+    VNFM class contains the information about VNFMs.
+    Each VNFM uses 'cores' and 'memory' from the deployed node
+    and can manage the 'capacity' functions.
+    """
+
     cores: int
     memory: int
     capacity: int
