43699a9e0432a5a7a8ccd3eee306c67c7c52b24e
Parham Alvani <parham.alvani@gmail.com>
2020-04-30 15:21:04 +0430

    feat: Add More Tests for Bari

--- jsd_mp/bari/test_bari.py
+++ jsd_mp/bari/test_bari.py
@@ -1,9 +1,43 @@
-from domain import Placement, Type, Node, Topology, Link, Chain, VNFM
+from domain import Placement, Type, Node, Topology, Link, Chain, VNFM, Direction
 from config import Config
 from .bari import Bari
 
 
 class TestBari:
+    def test_complex_chain(self):
+        fw = Type("fw", 2, 2)
+        ingress = Type("in", 0, 0, Direction.INGRESS)
+        egress = Type("out", 0, 0, Direction.EGRESS)
+        svr = Type("svr", 2, 2)
+
+        ch = Chain("ch", 100)
+
+        ch.add_function(ingress)
+        ch.add_function(fw)
+        ch.add_function(svr)
+        ch.add_function(egress)
+
+        ch.add_link(0, 1, Link(10))
+        ch.add_link(1, 2, Link(5))
+        ch.add_link(2, 3, Link(5))
+
+        topo = Topology()
+        topo.add_node("s1", Node(2, 2))
+        topo.add_node("s2", Node(2, 2, direction=Direction.NONE))
+        topo.add_node("s3", Node(2, 2, direction=Direction.INGRESS))
+        topo.add_link("s1", "s2", Link(20))
+        topo.add_link("s2", "s1", Link(20))
+        topo.add_link("s1", "s3", Link(20))
+
+        vnfm = VNFM(2, 2, 2, 2, 2, 2)
+
+        cfg = Config(types=[fw], chains=[ch], topology=topo, vnfm=vnfm)
+
+        bari = Bari(cfg)
+        pls = bari.solve()
+
+        assert len(pls) == 0
+
     def test_cost_of_chain(self):
         fw = Type("fw", 2, 2)
 
