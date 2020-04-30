9948f2b54d290b011b14f3d35928d813c79880bd
Parham Alvani <parham.alvani@gmail.com>
2020-04-30 17:59:10 +0430

    feat: Add Management Placement

--- jsd_mp/domain/placement.py
+++ jsd_mp/domain/placement.py
@@ -1,8 +1,43 @@
 from .nfv import Chain
+from .vnfm import VNFM
 from .topology import Topology, Link, Node
 
 import dataclasses
 import typing
+
+
+@dataclasses.dataclass(frozen=True)
+class ManagementPlacement:
+    chain: Chain
+    vnfm: VNFM
+    management_node: str = ""
+    management_links: typing.List[
+        typing.List[typing.Tuple[str, str]]
+    ] = dataclasses.field(default_factory=list)
+
+    def __post_init__(self):
+        if len(self.chain.functions) != len(self.management_links):
+            raise ValueError("Each node of chain must have management link")
+
+    def apply_on_topology(self, topo: Topology):
+        node = topo.nodes[self.management_node]
+        topo.update_node(
+            self.management_node,
+            Node(
+                cores=node.cores - self.vnfm.cores,
+                memory=node.memory - self.vnfm.memory,
+                direction=node.direction,
+                vnf_support=node.vnf_support,
+            ),
+        )
+
+        for path in self.management_links:
+            for step in path:
+                topo.update_link(
+                    step[0],
+                    step[1],
+                    Link(topo.links[step].bandwidth - self.vnfm.bandwidth),
+                )
 
 
 @dataclasses.dataclass(frozen=True)
