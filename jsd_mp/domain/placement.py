618814ce3505f10aa927fe535fa088dfd04821ad
Parham Alvani <parham.alvani@gmail.com>
2020-04-30 13:44:32 +0430

    feat: Add Apply to Topology

--- jsd_mp/domain/placement.py
+++ jsd_mp/domain/placement.py
@@ -1,4 +1,5 @@
 from .nfv import Chain
+from .topology import Topology, Link, Node
 
 import dataclasses
 import typing
@@ -18,3 +19,25 @@ class Placement:
         for p in self.chain.links:
             if p not in self.links:
                 raise ValueError("Placement must place every link of the chain")
+
+    def apply_on_topology(self, topo: Topology):
+        for i, n in enumerate(self.nodes):
+            node = topo.nodes[n]
+            t = self.chain.functions[i]
+            topo.update_node(
+                n,
+                Node(
+                    cores=node.cores - t.cores,
+                    memory=node.memory - t.memory,
+                    direction=node.direction,
+                    vnf_support=node.vnf_support,
+                ),
+            )
+
+        for (source, destination), path in self.links.items():
+            link = self.chain.links[(source, destination)]
+
+            for step in path:
+                topo.update_link(
+                    step[0], step[1], Link(topo.links[step].bandwidth - link.bandwidth)
+                )
