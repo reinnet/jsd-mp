bbdc9caea9d067d4aa855041eb1fdc2c6125b2b8
Parham Alvani <parham.alvani@gmail.com>
2020-05-01 08:51:37 +0430

    feat: Move Common Configuration to Solver

--- jsd_mp/bari/bari.py
+++ jsd_mp/bari/bari.py
@@ -129,27 +129,6 @@ class Bari(Solver):
             return None
         return Placement(chain, min_placement.nodes, min_placement.links)
 
-    def is_management_resource_available(
-        self, topology: Topology, manager: str, nodes: typing.List[str],
-    ) -> bool:
-        node = topology.nodes[manager]
-
-        if node.memory < self.vnfm.memory:
-            return False
-
-        if node.cores < self.vnfm.cores:
-            return False
-
-        r = topology.bfs(manager, self.vnfm.bandwidth)
-
-        for node in nodes:
-            for (destination, height) in r:
-                if destination == node and height <= self.vnfm.radius:
-                    break
-            else:
-                return False
-        return True
-
     def get_management_cost(
         self, topology: Topology, manager: str, nodes: typing.List[str],
     ) -> int:
@@ -168,41 +147,3 @@ class Bari(Solver):
         link: typing.Union[Link, None],
     ) -> int:
         return 0
-
-    @staticmethod
-    def is_resource_available(
-        topology: Topology,
-        previous: str,
-        current: str,
-        fn: Type,
-        link: typing.Union[Link, None],
-    ) -> bool:
-        """
-        Check the availability of current node for the given function.
-        This function consider the previous node for finding a path from it
-        to the current node.
-        """
-        node = topology.nodes[current]
-
-        if node.memory < fn.memory:
-            return False
-
-        if node.cores < fn.cores:
-            return False
-
-        if node.vnf_support is False:
-            return False
-
-        if fn.direction is Direction.INGRESS:
-            if node.direction is Direction.NONE or node.direction is Direction.EGRESS:
-                return False
-
-        if fn.direction is Direction.EGRESS:
-            if node.direction is Direction.NONE or node.direction is Direction.INGRESS:
-                return False
-
-        if previous != "" and link is not None:
-            if topology.path(previous, current, link.bandwidth) is None:
-                return False
-
-        return True
--- jsd_mp/solver/solver.py
+++ jsd_mp/solver/solver.py
@@ -1,5 +1,5 @@
 from config import Config
-from domain import Placement, ManagementPlacement
+from domain import Placement, ManagementPlacement, Topology, Link, Type, Direction
 
 import abc
 import typing
@@ -50,3 +50,62 @@ class Solver(abc.ABC):
         for (p, mp) in self.solution:
             profit += p.chain.fee
         return profit
+
+    def is_management_resource_available(
+        self, topology: Topology, manager: str, nodes: typing.List[str],
+    ) -> bool:
+        node = topology.nodes[manager]
+
+        if node.memory < self.vnfm.memory:
+            return False
+
+        if node.cores < self.vnfm.cores:
+            return False
+
+        r = topology.bfs(manager, self.vnfm.bandwidth)
+
+        for node in nodes:
+            for (destination, height) in r:
+                if destination == node and height <= self.vnfm.radius:
+                    break
+            else:
+                return False
+        return True
+
+    @staticmethod
+    def is_resource_available(
+        topology: Topology,
+        previous: str,
+        current: str,
+        fn: Type,
+        link: typing.Union[Link, None],
+    ) -> bool:
+        """
+        Check the availability of current node for the given function.
+        This function consider the previous node for finding a path from it
+        to the current node.
+        """
+        node = topology.nodes[current]
+
+        if node.memory < fn.memory:
+            return False
+
+        if node.cores < fn.cores:
+            return False
+
+        if node.vnf_support is False:
+            return False
+
+        if fn.direction is Direction.INGRESS:
+            if node.direction is Direction.NONE or node.direction is Direction.EGRESS:
+                return False
+
+        if fn.direction is Direction.EGRESS:
+            if node.direction is Direction.NONE or node.direction is Direction.INGRESS:
+                return False
+
+        if previous != "" and link is not None:
+            if topology.path(previous, current, link.bandwidth) is None:
+                return False
+
+        return True
