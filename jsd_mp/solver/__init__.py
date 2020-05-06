82d7172656275b5582f313d16bc335711151e282
Parham Alvani <parham.alvani@gmail.com>
2020-05-06 09:20:54 +0430

    feat: Add Documentation for Solver Module

--- jsd_mp/solver/__init__.py
+++ jsd_mp/solver/__init__.py
@@ -1,2 +1,8 @@
+"""
+Solver contains the basic structure for JSD-MP solvers.
+These solvers must use the given configuration and return the placement and manager placament
+for each chain.
+"""
+
 from .solver import Solver
 from .placement import PartialPlacement
