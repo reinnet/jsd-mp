023c2cdc4d2b184b0301d66a2d603813d63a25a9
Parham Alvani <parham.alvani@gmail.com>
2020-05-06 08:03:47 +0430

    feat: Complete Abu Lebdeh Implementation

--- jsd_mp/abu/abu.py
+++ jsd_mp/abu/abu.py
@@ -36,39 +36,52 @@ class Abu(Bari):
         for p, _ in placements:
             mp = self.place_manager(p.chain, self.topology, p)
             if mp is not None:
+                self.manage_by_node[mp.management_node] = self.manage_by_node.get(
+                    mp.management_node, 0
+                ) + len(ch)
+
                 active_vnfms.add(mp.management_node)
                 mp.apply_on_topology(self.topology)
                 actual_placements.append((p, mp))
             else:
-                pass  # TODO: put the chain's resources back
+                p.revert_on_topology(self.topology)
 
         # in each iteration we try to improve the manager placement
         for _ in range(self.n_iter):
             # randomly switch chains between vnfms
-            p, mp = random.choice(actual_placements)
+            index = random.randint(0, len(actual_placements))
+            p, mp = actual_placements[index]
             vnfm = random.choice(list(active_vnfms - set(mp.management_node)))
+            mp.revert_on_topology(self.topology)
             if self.is_management_resource_available(self.topology, vnfm, p.nodes):
-                pass
-                # TODO: revert current manager placement
-                # apply new manager placement
+                paths = []
+                for n in p.nodes:
+                    path = self.topology.path(vnfm, n, self.vnfm.bandwidth)
+                    if path is not None:
+                        paths.append(path)
+                mp.apply_on_topology(self.topology)
+                actual_placements[index] = (
+                    p,
+                    ManagementPlacement(p.chain, self.vnfm, vnfm, paths),
+                )
         return actual_placements
 
     def place_manager(
         self, chain: Chain, topology: Topology, placement: Placement
     ) -> typing.Union[ManagementPlacement, None]:
-        min_node = ""
+        node = ""
 
         for n in set(placement.nodes):
             if self.is_management_resource_available(topology, n, placement.nodes):
-                min_node = n
+                node = n
                 break
         else:
             return None
 
         paths = []
         for n in placement.nodes:
-            path = topology.path(min_node, n, self.vnfm.bandwidth)
+            path = topology.path(node, n, self.vnfm.bandwidth)
             if path is not None:
                 paths.append(path)
 
-        return ManagementPlacement(chain, self.vnfm, min_node, paths)
+        return ManagementPlacement(chain, self.vnfm, node, paths)
