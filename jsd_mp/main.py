b54c11a4bddba2f0b1cf40350981c773cfd705fa
Parham Alvani <parham.alvani@gmail.com>
2020-05-06 09:20:27 +0430

    feat: Better Main

--- jsd_mp/main.py
+++ jsd_mp/main.py
@@ -1,9 +1,14 @@
-from config import load
-from bari import Bari
+"""
+Entry point for JSD-MP (Joint Service Deployment - Manager Placement)
+Here you can run solvers of this package on your network requests.
+"""
 
-import click
 import logging
 import time
+import click
+
+from config import load
+from bari import Bari
 
 
 @click.command()
@@ -11,6 +16,9 @@ import time
 @click.option("--verbose", "-v", default=False, is_flag=True)
 @click.option("--placement", "-p", default=False, is_flag=True)
 def bari(config, verbose, placement):
+    """
+    Bari command run the Bari solver.
+    """
     if verbose is True:
         logging.basicConfig(level=logging.INFO)
 
@@ -19,33 +27,36 @@ def bari(config, verbose, placement):
     end = time.time()
     print(f"load configuration takes {end - start} seconds")
 
-    b = Bari(cfg)
+    solver = Bari(cfg)
     start = time.time()
-    b.solve()
+    solver.solve()
     end = time.time()
     print(f"bari solution takes {end - start} seconds")
 
     if placement is True:
-        for (p, pm) in b.solution:
+        for (p, pm) in solver.solution:
             print(f"{p.chain.name:=^25}")
-            for i, n in enumerate(p.nodes):
-                print(f"funcion-{i} [{p.chain.functions[i].name}] is placed on {n}")
+            for i, node in enumerate(p.nodes):
+                print(f"funcion-{i} [{p.chain.functions[i].name}] is placed on {node}")
 
-            for (f, t), path in p.links.items():
-                print(f"function-{f} -> function-{t} is placed on the following links:")
-                for (f, t) in path:
-                    print(f"\t{f} -> {t}")
+            for (from_function, to_function), path in p.links.items():
+                print(
+                    f"function-{from_function} -> function-{to_function} "
+                    "is placed on the following links:"
+                )
+                for (source, sink) in path:
+                    print(f"\t{source} -> {sink}")
 
             print(f"manage by {pm.management_node}")
 
             for i, path in enumerate(pm.management_links):
                 print(f"management route of function-{i}:")
-                for (f, t) in path:
-                    print(f"\t{f} -> {t}")
+                for (source, sink) in path:
+                    print(f"\t{source} -> {sink}")
         print()
 
-    print(f"Placement has profit: {b.profit} and cost: {b.cost}")
-    print(f"{len(b.solution)} has been placed successfully from {len(cfg.chains)}")
+    print(f"Placement has profit: {solver.profit} and cost: {solver.cost}")
+    print(f"{len(solver.solution)} has been placed successfully from {len(cfg.chains)}")
 
 
 if __name__ == "__main__":
