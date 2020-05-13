0591f09e52745aeff591688b952524b95974134f
Parham Alvani <parham.alvani@gmail.com>
2020-05-13 10:24:59 +0430

    feat: Even Better Random Bari with Tabu Search

--- jsd_mp/rari/rari.py
+++ jsd_mp/rari/rari.py
@@ -7,20 +7,25 @@ from domain import (
 )
 
 import typing
+import random
+import math
+import itertools
 
 
 class Rari(Solver):
+    n_iter: int = 100
+
     def _solve(self) -> typing.List[typing.Tuple[Placement, ManagementPlacement]]:
         placements: typing.List[typing.Tuple[Placement, ManagementPlacement]] = []
 
-        random = Random(Config({}, self.chains, self.vnfm, self.topology))
-        placements.extend(random.solve())
-        for node, count in random.manage_by_node.items():
+        rnd_index = math.ceil(len(self.chains) / 2)
+
+        rnd = Random(Config({}, self.chains[:rnd_index], self.vnfm, self.topology))
+        placements.extend(rnd.solve())
+        for node, count in rnd.manage_by_node.items():
             self.manage_by_node[node] = count
 
-        self.logger.info(
-            "Random place %d chains of %d", len(placements), len(self.chains)
-        )
+        self.logger.info("Random place %d chains of %d", len(placements), rnd_index)
         placed_chains = [p.chain for (p, _) in placements]
 
         bari = Bari(
@@ -41,4 +46,63 @@ class Rari(Solver):
             len(self.chains) - len(placed_chains),
         )
 
+        # in each iteration we try to improve the manager placement
+        for _ in range(self.n_iter):
+            current_cost = self.cost
+
+            # randomly switch chains between vnfms
+            index = random.randint(0, len(placements) - 1)
+            p, mp = placements[index]
+            # each node can be a manager if it has the required resources
+            vnfm = random.choice(
+                list(
+                    set(self.topology.nodes)
+                    - (set(mp.management_node) if mp is not None else set())
+                )
+            )
+
+            # revert the current manager placement
+            self.revert_management(mp)
+
+            # find new manager placement
+            manageable_functions = list(
+                itertools.compress(p.nodes, p.chain.manageable_functions)
+            )
+            if self.is_management_resource_available(
+                self.topology, vnfm, manageable_functions
+            ):
+                paths = []
+                for n in manageable_functions:
+                    path = self.topology.path(vnfm, n, self.vnfm.bandwidth)
+                    if path is not None:
+                        paths.append(path)
+                new_mp = ManagementPlacement(p.chain, self.vnfm, vnfm, paths)
+
+                # apply new manager placement
+                self.apply_management(new_mp)
+
+                if self.cost > current_cost:
+                    self.revert_management(new_mp)
+                    self.apply_management(mp)
+                else:
+                    # update the placement
+                    placements[index] = (
+                        p,
+                        new_mp,
+                    )
+            else:
+                self.apply_management(mp)
+
         return placements
+
+    def apply_management(self, mp: ManagementPlacement):
+        mp.apply_on_topology(self.topology)
+        self.manage_by_node[mp.management_node] = self.manage_by_node.get(
+            mp.management_node, 0
+        ) + len(mp.management_links)
+
+    def revert_management(self, mp: ManagementPlacement):
+        mp.revert_on_topology(self.topology)
+        self.manage_by_node[mp.management_node] = self.manage_by_node.get(
+            mp.management_node, 0
+        ) - len(mp.management_links)
