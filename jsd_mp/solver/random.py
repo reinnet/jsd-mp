b6d5521743f38f994afc27ce96fdae1dbfb10470
Parham Alvani <parham.alvani@gmail.com>
2020-05-13 09:31:24 +0430

    feat: Random Solver is Here :dancer:

---
+++ jsd_mp/solver/random.py
@@ -0,0 +1,90 @@
+from .solver import Solver
+from .placement import PartialPlacement
+from domain import (
+    Placement,
+    ManagementPlacement,
+    Type,
+    Node,
+    Topology,
+    Link,
+    Chain,
+    VNFM,
+)
+
+import typing
+import random
+import itertools
+
+
+class Random(Solver):
+    def _solve(self) -> typing.List[typing.Tuple[Placement, ManagementPlacement]]:
+        placements: typing.List[typing.Tuple[Placement, ManagementPlacement]] = []
+
+        for chain in self.chains:
+            topology = self.topology
+            pp = PartialPlacement(chain)
+
+            for i, function in enumerate(chain.functions):
+                n: str = random.choice(
+                    [
+                        name
+                        for (name, node) in topology.nodes.items()
+                        if node.memory >= function.memory
+                        and node.cores >= function.cores
+                        and node.vnf_support
+                    ]
+                )
+
+                if self.is_resource_available(
+                    topology,
+                    "" if i == 0 else pp.nodes[i - 1],
+                    n,
+                    function,
+                    None if i == 0 else chain.links[(i - 1, i)],
+                ):
+                    if i > 0:
+                        path = topology.path(
+                            pp.nodes[i - 1], n, chain.links[(i - 1, i)].bandwidth,
+                        )
+                        if path is not None:
+                            pp.append(n, path)
+                        else:
+                            break
+                    else:
+                        pp.append(n, None)
+                    topology = pp.apply_on_topology(self.topology)
+                else:
+                    break
+            else:
+                management_node: str = random.choice(
+                    [
+                        name
+                        for (name, node) in topology.nodes.items()
+                        if node.memory >= self.vnfm.memory
+                        and node.cores >= self.vnfm.cores
+                        and node.vnf_support
+                    ]
+                )
+                if not self.is_management_resource_available(
+                    topology,
+                    management_node,
+                    list(itertools.compress(pp.nodes, chain.manageable_functions)),
+                ):
+                    continue
+                management_paths = []
+                for n in itertools.compress(pp.nodes, chain.manageable_functions):
+                    path = self.topology.path(management_node, n, self.vnfm.bandwidth)
+                    if path is not None:
+                        management_paths.append(path)
+                    else:
+                        break
+                else:
+                    p = Placement(chain, pp.nodes, pp.links)
+                    p.apply_on_topology(self.topology)
+                    mp = ManagementPlacement(
+                        chain, self.vnfm, management_node, management_paths
+                    )
+                    mp.apply_on_topology(self.topology)
+                    placements.append((p, mp))
+
+        return placements
--- jsd_mp/solver/test_solver.py
+++ jsd_mp/solver/test_solver.py
@@ -1,7 +1,5 @@
-from .solver import Solver
+from .random import Random
 from domain import (
-    Placement,
-    ManagementPlacement,
     Type,
     Node,
     Topology,
@@ -11,46 +9,10 @@ from domain import (
 )
 from config import Config
 
-import typing
 import random
-import itertools
 
 
-class DummySolver(Solver):
-    def _solve(self) -> typing.List[typing.Tuple[Placement, ManagementPlacement]]:
-        placements: typing.List[typing.Tuple[Placement, ManagementPlacement]] = []
-
-        for chain in self.chains:
-            nodes = []
-            links = {}
-            for function in chain.functions:
-                n = random.choice(list(self.topology.nodes.keys()))
-                nodes.append(n)
-            for (source, destination), link in chain.links.items():
-                r = self.topology.path(
-                    nodes[source], nodes[destination], link.bandwidth
-                )
-                if r is not None:
-                    links[(source, destination)] = r
-            management_node = random.choice(list(self.topology.nodes.keys()))
-            management_paths = []
-            for n in itertools.compress(nodes, chain.manageable_functions):
-                path = self.topology.path(management_node, n, self.vnfm.bandwidth)
-                if path is not None:
-                    management_paths.append(path)
-            placements.append(
-                (
-                    Placement(chain, nodes, links),
-                    ManagementPlacement(
-                        chain, self.vnfm, management_node, management_paths
-                    ),
-                )
-            )
-
-        return placements
-
-
-class TestDummySolver:
+class TestRandomSolver:
     def test_simple_chain(self):
         fw = Type("fw", 2, 2)
 
@@ -58,30 +20,34 @@ class TestDummySolver:
 
         ch.add_function(fw)
         ch.add_function(fw)
-        ch.add_function(fw)
 
         ch.add_link(0, 1, Link(10))
-        ch.add_link(1, 2, Link(10))
 
         topo = Topology()
         topo.add_node("s1", Node(2, 2))
         topo.add_node("s2", Node(2, 2))
+        topo.add_node("s3", Node(2, 2))
         topo.add_link("s1", "s2", Link(20))
         topo.add_link("s2", "s1", Link(20))
+        topo.add_link("s3", "s1", Link(20))
+        topo.add_link("s3", "s2", Link(20))
 
         vnfm = VNFM(2, 2, 2, 2, 2, 2)
 
         cfg = Config(types=[fw], chains=[ch], topology=topo, vnfm=vnfm)
 
         random.seed(a=1378)
-        pls = DummySolver(cfg).solve()
+        pls = Random(cfg).solve()
+
+        assert len(pls) == 1
 
         assert pls[0][0].chain == ch
-        assert len(pls[0][0].nodes) == 3
-        assert len(pls[0][0].links) == 2
+        assert len(pls[0][0].nodes) == 2
+        assert len(pls[0][0].links) == 1
 
-        assert pls[0][0].nodes == ["s1", "s1", "s2"]
+        assert pls[0][0].nodes == ["s1", "s2"]
         assert pls[0][0].links == {
-            (0, 1): [],
-            (1, 2): [("s1", "s2")],
+            (0, 1): [("s1", "s2")],
         }
+
+        assert pls[0][1].management_node == "s3"
