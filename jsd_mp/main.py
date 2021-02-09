ca757e393fead100c879926f505850eb844bf026
Parham Alvani <parham.alvani@gmail.com>
2021-02-09 09:48:38 +0330

    feat: Run in Multiple Process

--- jsd_mp/main.py
+++ jsd_mp/main.py
@@ -5,11 +5,50 @@ Here you can run solvers of this package on your network requests.
 
 import logging
 import time
-from typing import List
+from typing import List, Tuple
+from multiprocessing import Pool
 import click
 
 from result import Result, report_csv
-from config import load
+from domain import Placement, ManagementPlacement
+from config import load, Config
+
+
+def execute(
+    run: int,
+    name: str,
+    options: List[Tuple[str, str]],
+    cfg: Config,
+) -> Tuple[Result, List[Tuple[Placement, ManagementPlacement]]]:
+    """
+    Execute a solver run into isolated process to improve performance
+    """
+    # import solver based on given name, for example in case of bari
+    # we import Bari from bari package.
+    solver = getattr(__import__(name), name.title())(cfg)
+
+    # pass options to the solver.
+    # please note that these options are class propeties
+    # on solver.
+    for option, value in options:
+        setattr(solver, option, value)
+
+    start = time.time()
+    solver.solve()
+    end = time.time()
+
+    return (
+        Result(
+            run=run,
+            solver=name,
+            elapsed_time=end - start,
+            profit=solver.profit,
+            cost=solver.cost,
+            number_of_chains=len(cfg.chains),
+            number_of_placed_chains=len(solver.solution),
+        ),
+        solver.solution,
+    )
 
 
 @click.command()
@@ -64,52 +103,46 @@ def main(config, verbose, show_placement, solvers, runs, options):
     print(f"load configuration takes {end - start} seconds")
 
     results: List[Result] = []
+    async_results = []
 
-    for run in range(runs):
-        print()
-        print(f"{' run {} '.format(run + 1):*^50}")
-        print()
-        for name in solvers:
-            # import solver based on given name, for example in case of bari
-            # we import Bari from bari package.
-            solver = getattr(__import__(name), name.title())(cfg)
-
-            # pass options to the solver.
-            # please note that these options are class propeties
-            # on solver.
-            for option, value in options:
-                setattr(solver, option, value)
-
-            start = time.time()
-            solver.solve()
-            end = time.time()
-            print(f"{name} solution takes {end - start} seconds")
-
-            if show_placement is True:
-                for (placement, manager_placement) in solver.solution:
-                    print(f"{placement.chain.name:=^25}")
-                    print(placement)
-                    print(manager_placement)
-                print()
-
-            print(
-                f"Placement has profit: {solver.profit} and cost: {solver.cost}"
-            )
-            print(
-                f"{len(solver.solution)} has been placed"
-                f" successfully from {len(cfg.chains)}"
-            )
-            results.append(
-                Result(
-                    run=run,
-                    solver=name,
-                    elapsed_time=end - start,
-                    profit=solver.profit,
-                    cost=solver.cost,
-                    number_of_chains=len(cfg.chains),
-                    number_of_placed_chains=len(solver.solution),
+    with Pool(8) as pool:
+        for run in range(runs):
+            for name in solvers:
+                async_result = pool.apply_async(
+                    execute,
+                    [
+                        run,
+                        name,
+                        options,
+                        cfg,
+                    ],
                 )
-            )
+                async_results.append(async_result)
+
+    for async_result in async_results:
+        result, solution = async_result.get()
+
+        print()
+        print(f"{' run {} '.format(result.run + 1):*^50}")
+        print()
+
+        print(f"{result.name} solution takes {result.elapsed_time} seconds")
+
+        if show_placement is True:
+            for (placement, manager_placement) in solution:
+                print(f"{placement.chain.name:=^25}")
+                print(placement)
+                print(manager_placement)
+            print()
+
+        print(f"Placement has profit: {result.profit} and cost: {result.cost}")
+        print(
+            f"{len(solution)} has been placed"
+            f" successfully from {len(cfg.chains)}"
+        )
+
+        results.append(result)
+
     report_csv(results)
 
 
